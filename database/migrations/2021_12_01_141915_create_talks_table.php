3d63f43916cf757b76ee6b6e7ba305e18edc102d
yuyacode <ny410144@gmail.com>
2021-12-18 20:47:41 +0900

    矛盾解消

--- database/migrations/2021_12_01_141915_create_talks_table.php
+++ database/migrations/2021_12_01_141915_create_talks_table.php
@@ -15,7 +15,7 @@ class CreateTalksTable extends Migration
     {
         Schema::create('talks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title', 50);
+            $table->string('title', 100);
             $table->integer('posts_number')->default(0);
             $table->timestamp('created_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'));
