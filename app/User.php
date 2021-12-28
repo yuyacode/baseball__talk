5ddb34380be8b7a23a2b75cba82d7c003dfbd56b
yuyacode <ny410144@gmail.com>
2021-12-28 10:46:00 +0900

    S3 500Errorの解消

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'profile', 'team_id',
+        'name', 'email', 'password', 'profile', 'team_id', 'profile_image',
     ];
 
     /**
