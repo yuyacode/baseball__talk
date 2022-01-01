a6ab9dbdee9c9c7ccf00d3fc0c65b28a228e0e38
yuyacode <ny410144@gmail.com>
2022-01-01 19:37:01 +0900

    HTML/CSS（各種一覧画面）

--- resources/views/talks/index_popular.blade.php
+++ resources/views/talks/index_popular.blade.php
@@ -7,37 +7,45 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>baseball talk</title>
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     </head>
-    <body>
+    <body class="body">
         @section('content')
-        <div class="container">
+        <div class="container_talks">
             <!-- 人気のトーク一覧 -->
-            <p>人気のトーク一覧</p>
-            <!-- 対象期間 -->
-            <p>（{{ $term }}）</p>
-            <!-- 期間を指定する -->
-            <div>
-                <p>期間を指定する</p>
-                @if($term == '今月')
-                <p>今月</p>
-                <p><a href="/talks_popular_week">今週</a></p>
-                <p><a href="/talks_popular_today">今日</a></p>
-                @elseif($term == '今週')
-                <p><a href="/talks_popular_month">今月</a></p>
-                <p>今週</p>
-                <p><a href="/talks_popular_today">今日</a></p>
-                @else
-                <p><a href="/talks_popular_month">今月</a></p>
-                <p><a href="/talks_popular_week">今週</a></p>
-                <p>今日</p>
-                @endif
+            <h2 class="talks_title">人気のトーク一覧</h2>
+            <!-- 期間 -->
+            <div class="period_specify--wrapper">
+                <!-- 対象期間 -->
+                <p class="target_period">対象期間：{{ $term }}</p>
+                <!-- 期間を指定する -->
+                <div class="period_specify--title">
+                    <p>期間を指定する</p>
+                </div>
+                <div class="period_specify">
+                    @if($term == '今月')
+                    <p class="period_specify--candidate"><a href="/talks_popular_today">今日</a></p>
+                    <p class="period_specify--candidate"><a href="/talks_popular_week">今週</a></p>
+                    <p class="period_specify--subject">今月</p>
+                    @elseif($term == '今週')
+                    <p class="period_specify--candidate"><a href="/talks_popular_today">今日</a></p>
+                    <p class="period_specify--subject">今週</p>
+                    <p class="period_specify--candidate"><a href="/talks_popular_month">今月</a></p>
+                    @else
+                    <p class="period_specify--subject">今日</p>
+                    <p class="period_specify--candidate"><a href="/talks_popular_week">今週</a></p>
+                    <p class="period_specify--candidate"><a href="/talks_popular_month">今月</a></p>
+                    @endif
+                </div>
             </div>
             <div class="talks">
                 @foreach($talks as $talk)
                 <div class="talk">
-                    <h2 class="talk--title"><a href="talks/{{ $talk->id }}">{{ $talk->title }}</a></h2>
-                    <p class="talk--posts_number">投稿数：{{ $talk->posts_number }}</p>
-                    <p class="talk--created_at">{{ $talk->created_at }}</p>
+                    <div class="talk_info">
+                        <h2 class="talk-title"><a href="talks/{{ $talk->id }}">{{ $talk->title }}</a></h2>
+                        <p class="talk-posts_number">（投稿数：{{ $talk->posts_number }}）</p>
+                    </div>
+                    <p class="talk-created_at">（{{ $talk->created_at }}）</p>
                 </div>
                 @endforeach
             </div>
--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -7,17 +7,19 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>baseball talk</title>
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     </head>
-    <body>
+    <body class="body">
         @section('content')
-        <div class="container">
+        <div class="container_teams">
             <!-- 球団のトーク一覧 -->
-            <p>球団のトーク</p>
-            <p>球団を選ぶ</p>
+            <h2 class="teams_title">球団トーク</h2>
+            <p>球団トークとは、各球団のファンが集まり、会話を交わすスペースです。</p>
+            <p class="choose_teams">球団を選ぶ</p>
             <div class="teams">
                 @foreach($teams as $team)
                 <div class="team">
-                    <p class="team-title"><a href="/teams/{{ $team->id }}">{{ $team->title }}</a></p>
+                    <h2 class="team-title"><a href="/teams/{{ $team->id }}">{{ $team->title }}</a></h2>
                 </div>
                 @endforeach
             </div>
